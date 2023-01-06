3c5d9ab4023f4e68e66f901cb4c00b1548e69525
mc9ds <mc9ds@virginia.edu>
2023-01-06 18:28:30 -0500

    Added latlon dist calc method and top_n_nearest_airport method

--- src/main.py
+++ src/main.py
@@ -9,7 +9,112 @@ import json
 from util import load_headers, get_webpage, extract_listing_data, extract_next_page_url
 from TrustedHouseSitters import TrustedHouseSitter
 
-def test():
+from typing import overload
+import numpy as np
+import pandas as pd
+from tqdm import tqdm 
+import json
+import os
+from scraping import *
+
+
+def haversine(lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6371):
+    """
+    slightly modified version: of http://stackoverflow.com/a/29546836/2901002
+
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees or in radians)
+
+    All (lat, lon) coordinates must have numeric dtypes and be of equal length.
+
+    """
+    if to_radians:
+        lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
+
+    a = np.sin((lat2-lat1)/2.0)**2 + \
+        np.cos(lat1) * np.cos(lat2) * np.sin((lon2-lon1)/2.0)**2
+
+    return earth_radius * 2 * np.arcsin(np.sqrt(a))
+
+
+
+
+def top_n_nearest_airports(listing_lat,listing_lon, continent, n):
+
+    print('############## AIRPORT CSV OUTPUT ############')
+    airports = pd.read_csv('../airports.csv')
+
+    # Clean up continent data
+    airports['continent'] = airports['continent'].astype(str)
+    airports.loc[airports['continent']=='nan','continent'] = 'NA'
+    print(list(set(airports['continent'])))
+    
+
+    # Filter down to same continent
+    airports  = airports[airports['continent']==continent]
+
+    # Calculate distance between listing lat lon and airports
+
+
+
+    for index, row in airports.iterrows():
+
+        airports.loc[index,'dist'] = haversine(listing_lat, listing_lon, row['latitude_deg'], row['longitude_deg'])
+
+
+    # coords_1 = (listing_lat, listing_lon)
+    # coords_2 = (52.406374, 16.9251681)
+
+    # print(geopy.distance.geodesic(coords_1, [()]).km
+    # print( (airports['longitude_deg'] - listing_lon)**2)
+    # airports['dist_temp'] = (airports['longitude_deg'] - listing_lon)**2 + (airports['latitude_deg'] - listing_lat)**2
+    # airports['dist'] = airports['dist_temp'].apply(lambda x: math.sqrt(x))
+
+    #airports = airports.drop(columns = ['dist_temp'])
+    #print(airports['dist'].describe())
+
+    print(airports.sort_values(by = ['dist'])[['name', 'continent','latitude_deg', 'longitude_deg', 'local_code','iata_code','dist']])
+
+ 
+    #airports['dist'] = math.sqrt( ((airports['longitude_deg'] - ax)**2) + (airports['latitude_deg'] - ay)**2 )
+
+    # print(math.sqrt(((listing_lon - ax)**2)+((listing_lat-ay)**2)))
+    
+
+    
+    # print(list(set(airports['iso_country'])))
+    # print(airports.head(20))
+
+
+
+def generate_flight_dataframe(start_date, end_date, departure_location):
+    
+
+    #result = scrape_data(__,__, str(start_date), str(end_date))
+    result = scrape_data('JFK', 'IST', '2023-05-20', '2023-06-10')
+    #print(result)
+    #print(pd.DataFrame.from_dict(result))
+
+    # Check type of columns in dataframe
+    print([(c, type(pd.DataFrame.from_dict(result)[c][0])) for c in pd.DataFrame.from_dict(result).columns])
+    res_df = pd.to_datetime(pd.DataFrame.from_dict(result))
+
+    ## Cleaning dataframe columns
+
+    # Convert date columns to datetime type
+    res_df['Leave Date'] = pd.to_datetime(res_df['Leave Date'])
+    res_df['Return Date'] = pd.to_datetime(res_df['Return Date'])
+
+    # Convert 'Travel Time' column to int
+    res_df['Travel Time_hour_to_min'] = res_df['Travel Time'].apply(lambda x: int(str(x).split('hr')[0].strip())*60 )
+    res_df['Travel Time_min'] = res_df['Travel Time'].apply(lambda x: int(str(x).split('min')[0].split('hr')[1].strip()) if 'min' in str(x) else 0 )
+    res_df['Travel Time Converted'] = res_df['Travel Time_hour_to_min'] + res_df['Travel Time_min']
+    
+    return res_df
+
+
+
+def generate_THS_dataframe():
     ths = TrustedHouseSitter(headersFile="query_headers.json")
 
     allListings = ths.search_listings()
@@ -84,18 +189,18 @@ def test():
     res_df['Duration (Days)'] = res_df['End Date'] - res_df['Start Date']
     
 
-    print(res_df)
+    print(res_df[['Listing Location 1a', 'Listing Location 1b', 'Listing Location 1c', 'Listing Location 1d']])
 
 
 
 
-    unique = { each['id'] : each for each in tempList }.values()
-    print(len(unique))
-    # print(len(tempList))
-    tempMap = {}
-    for listing in tempList:
-        tempMap[listing["id"]] = listing["id"]
-    print(len(tempMap.keys()))
+    # unique = { each['id'] : each for each in tempList }.values()
+    # print(len(unique))
+    # # print(len(tempList))
+    # tempMap = {}
+    # for listing in tempList:
+    #     tempMap[listing["id"]] = listing["id"]
+    # print(len(tempMap.keys()))
         # print(hash(str(listing)))
     # print(allListings[9])
 
@@ -127,8 +232,26 @@ def main():
     # print(next_page)
 
 
+def main2():
+    
+    # Generate listings for TS
+    ths_listings = generate_THS_dataframe()
+    continent = ths_listings['Listing Location 1d'][0]
+
+
+    # Find ton n nearest airports to TS listing location
+
+    top_n_nearest_airports(lat, lon, continent, 2)
+
+    # Feed these airports to generate_flight_dataframe() and generate list of flights
+
+
+
 
 
 if __name__ == '__main__':
     # main()
-    test()
+    # test()
+    #generate_THS_dataframe()
+    top_n_nearest_airports(38.922250, -77.254710, 'NA', 2)
+    #main2()
